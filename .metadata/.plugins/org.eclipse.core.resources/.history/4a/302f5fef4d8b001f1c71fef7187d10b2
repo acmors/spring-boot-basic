9dccfb60e9ca9b9c7d1e1e605e6363b224261026
acmors <marcosantoniosilva047@gmail.com>
2024-10-15 20:39:10 -0300

    implementacao entity e repository

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4a/302f5fef4d8b001f1c71fef7187d10b2
@@ -0,0 +1,5 @@
+package com.acmors.festa.model;
+
+public class Convidado {
+
+}
--- gestao-festa/src/main/java/com/acmors/festa/model/Convidado.java
+++ gestao-festa/src/main/java/com/acmors/festa/model/Convidado.java
@@ -1,5 +1,46 @@
 package com.acmors.festa.model;
 
-public class Convidado {
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Convidado implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private Integer acompanhante;
+	
+	public Long getId() {
+		return id;
+	
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Integer getAcompanhante() {
+		return acompanhante;
+	}
+	
+	public void setAcompanhante(Integer acompanhante) {
+		this.acompanhante = acompanhante;
+	}
+	
+	
 }
